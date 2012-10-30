bcd5915153e30c560a8e9b19f66850da7e95978a
Kahlil Lechelt <kahlil@kahlil.co>
2012-10-30 16:58:52 +0100

    Introduced stolen code from Yeoman's compass task https://github.com/yeoman/yeoman/blob/master/cli/tasks/compass.js

--- tasks/lib/compass.js
+++ tasks/lib/compass.js
@@ -2,12 +2,37 @@ exports.init = function( grunt ) {
     'use strict';
 
     var exports = {};
-    var path = require( 'path' );
+    var path    = require( 'path' );
+    var _       = grunt.utils._;
 
     // Build CSS with compass compile
-    exports.buildCommand = function( data, done ) {
+    exports.optsToArgs = function( opts, done ) {
 
-        var src, dest, specify, matchedFiles;
+        var args = [];
+
+        Object.keys( opts ).forEach( function( el ) {
+            var val = opts[ el ];
+
+            el = el.replace( /_/g, '-' );
+
+            if ( val === true ) {
+                args.push( '--' + el );
+            }
+
+            if ( _.isString( val ) ) {
+                args.push( '--' + el, val );
+            }
+
+            if( _.isArray( val ) ) {
+                val.forEach(function( subval ) {
+                    args.push( '--' + el, subval );
+                });
+            }
+        });
+
+        return args;
+
+        /* var src, dest, specify, matchedFiles;
         var command        = "compass compile";
         var config         = data.config;
         var images         = data.images;
@@ -109,7 +134,7 @@ exports.init = function( grunt ) {
             command += ' -I ' + importPath;
         }
 
-        return command;
+        return command; */
     };
 
     // Output to the console
@@ -129,3 +154,60 @@ exports.init = function( grunt ) {
 
   return exports;
 };
+
+////
+/*
+module.exports = function( grunt ) {
+  'use strict';
+
+  var _ = grunt.util._;
+
+  function optsToArgs( opts ) {
+    var args = [];
+
+    Object.keys( opts ).forEach(function( el ) {
+      var val = opts[ el ];
+
+      el = el.replace( /_/g, '-' );
+
+      if ( val === true ) {
+        args.push( '--' + el );
+      }
+
+      if ( _.isString( val ) ) {
+        args.push( '--' + el, val );
+      }
+
+      if( _.isArray( val ) ) {
+        val.forEach(function( subval ) {
+          args.push( '--' + el, subval );
+        });
+      }
+    });
+
+    return args;
+  }
+
+  grunt.registerMultiTask( 'compass', 'Compass task', function() {
+    var cb = this.async();
+    var args = optsToArgs( this.options() );
+
+    var compass = grunt.util.spawn({
+      cmd: 'compass',
+      args: ['compile'].concat( args )
+    }, function( err, result, code ) {
+      if ( /not found/.test( err ) ) {
+        grunt.fail.fatal('You need to have Compass installed.');
+      }
+      // Since `compass compile` exits with 1 when it has nothing to compile,
+      // we do a little workaround by checking stdout which is then empty
+      // https://github.com/chriseppstein/compass/issues/993
+      cb( code === 0 || !result.stdout );
+    });
+
+    compass.stdout.pipe( process.stdout );
+    compass.stderr.pipe( process.stderr );
+  });
+};
+*/
+
